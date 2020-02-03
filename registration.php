249f7434b697eefe0ed586d471e6f1ad8acf0309
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-02-03 07:58:45 +0300

    basit hatalar düzeltildi

--- registration.php
+++ registration.php
@@ -8,14 +8,14 @@
 
 	echo "<form action=\"".getLink("registrationControl")."\" method=\"post\" >";
 ?>
-		Kullanıcı Adı: <input type="text" name="userNameField" required><br>
-		Şifre: <input type="password" name="passField" required><br><br>
-		Email: <input type="text" name="mailField" required><br>
-		İsim: <input type="text" name="fNameField" required><br>
-		Soyisim: <input type="text" name="lNameField" required><br>
+		Kullanıcı Adı: <input type="text" name="userNameField" required>*<br>
+		Şifre: <input type="password" name="passField" required>*<br><br>
+		Email: <input type="text" name="mailField" required>*<br>
+		İsim: <input type="text" name="fNameField" required>*<br>
+		Soyisim: <input type="text" name="lNameField" required>*<br>
 		Telefon No: <input type="text" name="pNoField" ><br>
 
-		<input type="submit" value="Giriş">
+		<input type="submit" value="Üye ol">
 <?php
 		echo "<a href=\"".getLink("login")."\">Geri Dön</a>";
 ?>
