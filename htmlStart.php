9e742d26d6204d72bef507fec89d10b4f1a3dc3b
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-02-03 07:56:56 +0300

    body'e dışarıdan stil eklendi

--- htmlStart.php
+++ htmlStart.php
@@ -11,4 +11,4 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link rel="stylesheet" type="text/css" href="CSS/CSSFile.css"/>
 		</head>
-	<body>
+	<body class="font1">
