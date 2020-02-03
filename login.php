1a5c35a7fbd6408b774efe0e884415976182f3c0
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-02-03 07:57:31 +0300

    login sayfasında basit stillendirme yapıldı

--- login.php
+++ login.php
@@ -4,13 +4,31 @@
 ?>
 
 
+<div class="loginBox">
+
 <?php
-	echo "<form action=\"".getLink("loginControl")."\" method=\"post\">";
+	echo "<form action=\"".getLink("loginControl")."\" method=\"post\" style=\"position: absolute;top: 50%;margin-top: -55px;\">";
 ?>
-		Email: <input type="text"  name="mailField" required><br>
-		Şifre: <input type="password"  name="passField" required><br><br>
-		<input type="submit" value="Giriş">
+	<center>
+	<div class="loginBoxRow">
+		<div class="loginBoxLeft">Email: </div>
+		<div class="loginBoxRight"><input type="text" name="mailField" required="" size="25"></div>
+	</div>
+		
+	<div class="loginBoxRow">
+		<div class="loginBoxLeft">Şifre: </div>
+		<div class="loginBoxRight"><input type="password" name="passField" required="" size="25"></div>
+	</div>
+	</center>
+		
+	<br>
+
+	<center>
+		<?php 
+			echo "<a href=\"".getLink("registration")."\">Yeni Üye</a>";
+		?>
+			<input type="submit" value="Giriş" style="margin-left: 20px;">
+	</center>
+
 	</form>
-<?php 
-	echo "<a href=\"".getLink("registration")."\">Yeni Üye</a>";
-?>
+</div>
