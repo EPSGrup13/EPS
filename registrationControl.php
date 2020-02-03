cfcd0db5f0fa86adc9a9545bd2332ddfefdfcd08
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-02-03 07:59:04 +0300

    null ve space koruması eklendi

--- registrationControl.php
+++ registrationControl.php
@@ -1,5 +1,6 @@
 <?php
-	include 'htmlStart.php';
+	include 'functions.php';
+	session_start();
 
 	$getUserName = $_POST["userNameField"];
 	$getPassword = $_POST["passField"];
@@ -8,6 +9,16 @@
 	$getLastName = $_POST["lNameField"];
 	$getPhoneNo = $_POST["pNoField"];
 
-	userRegistration($getUserName,convertPassToMD5($getPassword),$getEmail,$getFirstName,$getLastName,$getPhoneNo);
+	if(isNullorOnlySpace($getUserName) || isNullorOnlySpace($getPassword) || isNullorOnlySpace($getEmail) || isNullorOnlySpace($getFirstName) || isNullorOnlySpace($getLastName))
+	{
+		echo "Tüm alanları eksiksiz doldurunuz. Geril Yönlendiriliyorsunuz...";
+		redirectWithTimer("registration");
+	}
+	else
+	{
+		userRegistration($getUserName,convertPassToMD5($getPassword),$getEmail,$getFirstName,$getLastName,$getPhoneNo);
+	}
+
+
 
 ?>
