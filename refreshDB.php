744c6d55689a43615aaf910aad09964301bebdcb
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-02-27 23:49:11 +0300

    Günlük veritabanı güncellemesi için optimizasyon yapıldı

--- refreshDB.php
+++ refreshDB.php
@@ -1,20 +1,10 @@
 <?php
-	//include 'functions.php';
-	$servername = "localhost";
-	$username = "";
-	$password = "";
-	$dbname = "";
-
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	if ($conn->connect_error)
-	{
-	    die("DB bağlantı hatası, Sisteme bilgi gönderildi.");
-	}
-	$conn->set_charset("utf8");
+	include 'functions.php';
 
 	$timezone=0;
 	date_default_timezone_set('Europe/Istanbul');
 	$get_time = date("Y-m-d");
+	//$get_time=date("Y/m/d h:i:s a", time() + 3600*($timezone+date("I")));
 
 	$sql1 = "SELECT park_id FROM Park";
 	$result1 = $conn->query($sql1);
@@ -26,19 +16,16 @@
 			//echo $selectParkID;
 
 			$sql2 = "INSERT INTO parkStatus (recDate, park_id) VALUES ('$get_time','$selectParkID')";
-			if ($conn->query($sql2) === TRUE)
+			if (!($conn->query($sql2) === TRUE))
 			{
-				//dbFeedback();
-			}
-			else
-			{
-				//reportErrorLog("ReportErrorLog ErrorLog'a kayıt yaparken sorun oluştu", 1020);
+				reportErrorLog("ReportErrorLog ErrorLog'a kayıt yaparken sorun oluştu", 1020);
 			}
 		}
 	}
 	else
 	{
-		//reportErrorLog("getParkDetails fonksiyonunda veri çekilirken sorun oluştu", 1021);
+		reportErrorLog("getParkDetails fonksiyonunda veri çekilirken sorun oluştu", 1021);
 	}
+	dbFeedback();
 	$conn->close();
 ?>
