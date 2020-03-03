78e31fcab80c21ed3a8f4e8e48dbd2b1c776d146
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-03-03 22:47:25 +0300

    Araç detayları için kullanılan metod aktarıldı

--- profile.php
+++ profile.php
@@ -11,9 +11,23 @@
 echo "<div class=\"ou-profile\">";
 	echo "<div class=\"profile\">";
 		userProfile($_SESSION["person_id"]);
-	echo "</div>";
 
-	//echo "<br>";
+		//print_r(getWehicles($person_id));
+		$wehiclesArray = getWehicles($_SESSION["person_id"]);
+		if(is_array($wehiclesArray))
+		{
+			$lenWehiclesArray = count($wehiclesArray);
+			for($i = 0; $i < $lenWehiclesArray; $i++)
+			{
+				echo "Plaka: ".$wehiclesArray[$i]."<br>";
+			}
+		}
+		else
+		{
+			echo $wehiclesArray;
+		}
+			
+	echo "</div>";
 
 	echo "<div class=\"rsv\">";
 
