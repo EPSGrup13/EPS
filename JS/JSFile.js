cc44b5bef7110d3513d62f6b28d8e2df796246dd
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-03-09 23:24:34 +0300

    Yeni görünüme göre değişiklikler yapılmıştır

--- JS/JSFile.js
+++ JS/JSFile.js
@@ -35,17 +35,21 @@ function setStyles()
 {
 	sessionStorage.setItem("bgColor","#191919");
 	sessionStorage.setItem("hdrColor","#2d2d2d");
-	let getHeader = document.querySelector(".headerInline");
+	let getHeader = document.querySelector("nav");
 	let getFooter = document.querySelector(".footer");
 	let getContent = document.querySelector(".content");
 
-	if(sessionStorage.getItem("darkMode") == "true")
+	if(sessionStorage.getItem("darkMode") == "true") //darkmode kapanacak.
 	{
 		sessionStorage.setItem("darkMode", true);
 		sessionStorage.bg = 1;
 
 		document.body.style.color = "White";
-		getHeader.className = "headerInline";
+		getHeader.className = "nav";
+
+		document.body.style.background = "none";
+		document.body.style.backgroundColor = "#191919";
+
 		getHeader.style.backgroundColor = sessionStorage.getItem("hdrColor");
 		getFooter.style.backgroundColor = sessionStorage.getItem("hdrColor");
 		getContent.style.backgroundColor = sessionStorage.getItem("bgColor");
@@ -57,24 +61,26 @@ function setStyles()
 			aList[i].style.color = "White";
 		}
 	}
-	else
+	else //darkmode açılacak.
 	{
 		sessionStorage.setItem("darkMode", false);
 		sessionStorage.bg = 0;
 
+		document.body.style.background = "url('https://www.labdoo.org/sites/default/files/1604-lubuntu-default-wallpaper-neutral.png')";
+
 		document.body.style.color = "Black";
 		getHeader.removeAttribute("style");
 		getFooter.removeAttribute("style");
-		getHeader.className = "headerInline bg-dark";
-		getFooter.className = "footer bg-dark";
+		getHeader.className = "nav nav-bg";
+		getFooter.className = "footer footer-bg";
 		getContent.className = "content";
 		getContent.removeAttribute("style");
 
 		let aList = document.getElementsByTagName("a");
-		for(i=0;i<aList.length;i++)
+		for(i=3;i<aList.length;i++) //3. linkten itibaren başlayacak, 1. logo 2.link 3.link şeklinde. (header içinde)
 		{
 			//console.log(aList[i]);
-			aList[i].style.color = "Black";
+			aList[i].style.color = "White";
 		}
 	}
 }
