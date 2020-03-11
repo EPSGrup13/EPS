5f76a1e349af49c51ddd95d858afafd484ea17c2
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-03-12 00:57:20 +0300

    Görsel için düzeltme yapıldı

--- header.php
+++ header.php
@@ -5,45 +5,61 @@
 	session_start();
 ?>
 
-<div class="header">
+<nav class="nav-bg">
 
-	<div class="headerInline bg-dark">
+	<div class="headerInline">
 
-		<div class="linkBar">
-			<?php echo "<a href=\"".isDevelopmentModeOn()."\">Ana Sayfa</a>"; ?>
+		<div class="linkBar1">
+			<?php
+			echo "<a href=\"".isDevelopmentModeOn()."\"><img src=\"https://i.hizliresim.com/oyQbo2.png\" class=\"logo-size1\"></a>";
+			?>
+		</div>
+
+		<div class="linkBar2">
+			<?php
+			echo "<a href=\"".isDevelopmentModeOn()."\">Ana Sayfa</a>";
+			echo "<a href=\"".isDevelopmentModeOn()."contact\">İletişim</a>";
+			?>
 		</div>
 
 		<div class="userBar">
 
-<label class="switch">
-  <input type="checkbox" onclick="darkMode()" id="dm">
-  <span class="slider round"></span>
-</label>
+
 
 			
 		<?php
 			if(isSessionActive())
 			{
-				echo "<div class=\"up\"><a href=\"".isDevelopmentModeOn()."settings/profile\" class=\"uprofile\">".getSessionDisplayName()."</a>";
+				echo "<div class=\"up\">
+						<a href=\"".isDevelopmentModeOn()."settings/profile\" class=\"uprofile profileColor\">".getSessionDisplayName()."</a>";
 				if(getUserLevel() === 1) //Otopark sahibi ise.
 				{
 
 					echo "<div class=\"uprofile-content\">
 							<a href=\"".isDevelopmentModeOn()."parkdetayi\">Park Detayları</a>
-
-						  </div></div>";
+						  ";
 				}
 				else
 				{
 					echo "<div class=\"uprofile-content\">
 							<a href=\"javascript:void(0)\">link1</a>
-
-						  </div></div>";
+						  ";
 				}
 
-				echo "&nbsp;&nbsp;Bakiye: ".getUserBalance();
+				echo "
+						<div class=\"dba\">
+						Karanlık Mod
+							<label class=\"switch\">
+							  <input type=\"checkbox\" onclick=\"darkMode()\" id=\"dm\">
+							  <span class=\"slider round\"></span>
+							</label>
+						</div>
+					</div>
+				</div>";
+
+				echo "&nbsp;&nbsp;<font color=\"white\">Bakiye: ".getUserBalance(). "</font>";
 				
-				echo "&nbsp;&nbsp;<a href=\"".getLink("?logout")."\">Çıkış</a>";
+				echo "&nbsp;&nbsp;<a href=\"".getLink("?logout")."\" class=\"profileColor\" style=\"display:inline-block;\">Çıkış</a>";
 				if(isset($_GET["logout"]))
 				{
 					destroyUserSession();
@@ -51,11 +67,10 @@
 			}
 			else
 			{
-				echo "<a href=\"".getLink("login")."\">Giriş Yap</a>";
+				echo "<a href=\"".getLink("login")."\" class=\"profileColor\" style=\"display:inline-block;\">Giriş Yap</a>";
 			}
 		?>
 		</div>
 	</div>
 
-</div>
-
+</nav>
