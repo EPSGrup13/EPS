018493a80126fe359e9c487769fed5801bd7520b
Talha Keskin <50806631+talhakeskin@users.noreply.github.com>
2020-03-20 06:45:28 +0300

    Hata düzeltildi

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="/CSS/all.css">
     <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="icon" href="/images/logo-icon.png">
 </head>
 
 <body>
