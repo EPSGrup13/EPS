903d33d88a6e28af1e772e5b650d7c2fd20de275
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-03-21 06:17:13 +0300

    Hizalamalar için class değiştirildi

--- registration.php
+++ registration.php
@@ -10,12 +10,12 @@
         <div id="formkapsamkayit">
             <img id="formkapsamimgkayit" src="https://i.hizliresim.com/3gRYd4.png" />
             <h3 id="formkapsamhkayit">Kullanıcı Kayıt Formu</h3>
-                <div><input class="formkapsaminput" type="text" name="userName" placeholder="Kullanıcı Adı" /></div>
-                <div><input class="formkapsaminput" type="password" name="pass" placeholder="Şifre" /></div>
-                <div><input class="formkapsaminput" type="text" name="fName" placeholder="İsim" /></div>
-                <div><input class="formkapsaminput" type="text" name="lName" placeholder="Soyisim" /></div>
-                <div><input class="formkapsaminput" type="text" name="email" placeholder="E-mail" /></div>
-                <div><input class="formkapsaminput" type="tel" name="pNo" placeholder="Telefon Numarası 530-123-45-67" pattern="[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}"/></div>
+                <div class="formInput"><input class="formkapsaminput" type="text" name="userName" placeholder="Kullanıcı Adı" /></div>
+                <div class="formInput"><input class="formkapsaminput" type="password" name="pass" placeholder="Şifre" /></div>
+                <div class="formInput"><input class="formkapsaminput" type="text" name="fName" placeholder="İsim" /></div>
+                <div class="formInput"><input class="formkapsaminput" type="text" name="lName" placeholder="Soyisim" /></div>
+                <div class="formInput"><input class="formkapsaminput" type="text" name="email" placeholder="E-mail" /></div>
+                <div class="formInput"><input class="formkapsaminput" type="tel" name="pNo" placeholder="Telefon Numarası 530-123-45-67" pattern="[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}"/></div>
 
                 <div class="button-row">
                     <input type="submit" value="Üye Ol" class="submit-button" onclick="formValidation(); return false;">
