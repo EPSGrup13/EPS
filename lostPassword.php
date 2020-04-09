195a42a315f2b2303bd1ac61e30abc22831fab78
talhakeskin <50806631+talhakeskin@users.noreply.github.com>
2020-04-09 14:29:28 +0300

    Düzeltmeler yapıldı

--- lostPassword.php
+++ lostPassword.php
@@ -8,17 +8,17 @@
 
 <div class="content">
 	<div id="passwordResetBox">
-    <img id="passwordResetImg" src="https://i.hizliresim.com/Wo6WBE.png" />
-    <h3 id="passwordResetText">Şifremi Unuttum</h3>
-    <form method="POST" action="">
-		<div><input class="lostPwi" type="password" name="email" placeholder="E-mail"></div>
-		<div><button class="ep-btn" id="passwordResetButton" onclick="generateToken(); return false;">Şifre Yenile</button></div>
-    </form>
+	    <img id="passwordResetImg" src="https://i.hizliresim.com/Wo6WBE.png" />
+	    <h3 id="passwordResetText">Şifremi Unuttum</h3>
+	    <form method="POST" action="">
+			<div><input class="lostPwi" type="password" name="email" placeholder="E-mail"></div>
+			<div><button class="ep-btn" id="passwordResetButton" onclick="generateToken(); return false;">Şifre Yenile</button></div>
+	    </form>
 
-    <div class="alert">
-    <!--<span class="closebtn">×</span> -->
-        <p><img src="https://i.hizliresim.com/UoyHLn.png" class="parkDetailsImg">Sistemde kayıtlı olan mail adresine şifre sıfırlama linki gönderilecektir.</p>
-	</div>
+	    <div class="lp-alert">
+	    <!--<span class="closebtn">×</span> -->
+	        <img src="<?php echo isDevelopmentModeOn(); ?>images/info-icon.png" class="parkDetailsImg">Sistemde kayıtlı olan mail adresine şifre sıfırlama linki gönderilecektir.
+		</div>
     </div>
 </div>
 
@@ -31,4 +31,3 @@
 	
 	</body>
 </html>
-
