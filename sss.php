5a706f22922b9e9ffb4692a2f2e9bd2da30ab2ce
anilcanboga <60179891+anilcanboga@users.noreply.github.com>
2020-04-29 23:56:13 +0300

    SSS Sayfası Güncellemesi
    SSS sayfası değiştirildi.

--- sss.php
+++ sss.php
@@ -3,102 +3,120 @@
     includeExtContents("sss");
 ?>
 
-<body id="sss-body" onload="ckVersion();">
+<body id="sss-body" onload="ckVersion();" style="background-color: #e8e8e8; ">
 
-    <div class="sss-tum">
-        <div class="sss">
-            <h3 class="soru">Biz ne yapıyoruz <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> EPS, otoparkların fiyatlarını, yerlerini
-                    göstererek otoparklara rezervasyon yapılmasını sağlayan mobil uygulamadır. Arabanı park
-                    etmek için boşuna vakit harcamana gerek yok. Uygulamaya girerek otoparkların nerede
-                    olduğunu görebilir, rezervasyon yaparak yerini ayırabilir ve ödemeni cep telefonundan
+<div class="accordion-sss">
+        <ul class="ul-sss">
+            <li>
+                <h4 class="sss-question" style="border-top-left-radius: 10px;">Biz ne yapıyoruz?</h4>
+                <p class="sss-answer">EPS, otoparkların fiyatlarını, yerlerini göstererek otoparklara rezervasyon
+                    yapılmasını sağlayan mobil uygulamadır. Arabanı park etmek için boşuna vakit harcamana gerek yok.
+                    Uygulamaya girerek otoparkların nerede olduğunu görebilir, rezervasyon yaparak yerini ayırabilir ve ödemeni cep telefonundan
                     yaparak arabanı rahatça park edebilirsin.</p>
-            </div>
-            <h3 class="soru">Uygulama ücretli mi <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> Uygulamayı ücretsiz olarak cep
-                    telefonunuza indirebilirsiniz.
-                </p>
-            </div>
-            <h3 class="soru">Hangi bölgede hizmet veriyoruz <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> Şu an için İstanbul içerisinde hizmet vermekteyiz. Diğer
+            </li>
+
+            <li>
+                <h4 class="sss-question">EPS ücretli mi?</h4>
+                <p class="sss-answer">EPS'yi ücretsiz olarak cep telefonunuza indirebilirsiniz.</p>
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Hangi bölgede hizmet veriyoruz?</h4>
+                <p class="sss-answer">Şu an için İstanbul içerisinde hizmet vermekteyiz. Diğer
                     iller için çalışmalarımız devam
-                    etmektedir.
-                </p>
-            </div>
-            <h3 class="soru">Çalışma saati nedir <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> EPS sizlere 7/24 hizmet sağlamaktadır. İstediğiniz
+                    etmektedir.</p>
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Çalışma saati nedir? </h4>
+                <p class="sss-answer">EPS sizlere 7/24 hizmet sağlamaktadır. İstediğiniz
                     zaman müşteri hizmetlerimizden
                     yardım alabilirsiniz.</p>
-            </div>
-            <h3 class="soru">Otopark ücretini nasıl ödeyeceğim <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> Otoparka rezervasyon yaptığınız anda otopark tarifesinde
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Otopark ücretini nasıl ödeyeceğim?</h4>
+                <p class="sss-answer">Otoparka rezervasyon yaptığınız anda otopark tarifesinde
                     bulunan minimum ücret ve kaç
                     saat sonrasına rezervasyon yaptığınıza bağlı olarak rezervasyon ücreti alınır.
                     Otoparktan çıkış yapacağınızda içeride kaldığınız toplam süreye bağlı olarak +2 saat fazladan ödeme
                     gerçekleştirilir. 2 saat güvence bedeli, kullanıcı zamanında çıkış yaptığı taktirde bakiyesine geri
                     yansıtılmaktadır. </p>
-            </div>
-            <h3 class="soru">Otoparka nasıl rezervasyon yapabilirim <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> EPS uygulaması ile otoparka giriş yapmanızdan minimum
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Otoparka nasıl rezervasyon yapabilirim?</h4>
+                <p class="sss-answer">EPS uygulaması ile otoparka giriş yapmanızdan minimum
                     10 dakika öncesine kadar
                     rezervasyon yapabilirsiniz. Otoparkın rezervasyon ücreti ödemesi gerçekleştirilince
                     otoparktaki yeriniz ayrılmış olur.</p>
-            </div>
-            <h3 class="soru">Nakit ödeme yapılabiliyor mu <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> EPS rezervasyon ücretini nakit ödeme ile kabul
+            </li>
+
+            <li>
+                <h4 class="sss-question">Nakit ödeme yapılabiliyor mu?</h4>
+                <p class="sss-answer">EPS rezervasyon ücretini nakit ödeme ile kabul
                     edilmemektedir. Tüm ödemelerinizi
                     yüksek güvenlik önlemleri ile korunan online sistemimiz üzerinden
                     gerçekleştirebilirsiniz.</p>
-            </div>
-            <h3 class="soru">Kartımın limiti yetersiz. Otopark ücretini nasıl
-                ödemeliyim <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> Otopark rezervasyonu yapabilmek için farklı kartla ödeme
+            </li>
+
+            <li>
+                <h4 class="sss-question">Kartımın limiti yetersiz. Otopark ücretini nasıl
+                    ödemeliyim?</h4>
+                <p class="sss-answer">Otopark rezervasyonu yapabilmek için farklı kartla ödeme
                     yapabilirsiniz. En fazla 2 farklı banka kartı ile ödeme yapabilirsiniz.</p>
-            </div>
-            <h3 class="soru">Rezervasyon iptal işlemlerini nasıl
-                gerçekleştirebilirim <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> E-Park uygulamasında rezervasyon iptali sürecini
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Rezervasyon iptal işlemlerini nasıl
+                    gerçekleştirebilirim?</h4>
+                <p class="sss-answer">E-Park uygulamasında rezervasyon iptali sürecini
                     rezervasyon saatine 10 dakika kalıncaya kadar iptal edebilirler. Otoparka geç
-                    kalma durumunda rezervasyon işleminiz hiçbir şekilde iptal edilmez.
-                </p>
-            </div>
-            <h3 class="soru">Otopark fiyatları güncel midir <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> Otopark fiyatları doluluk oranına, otoparkın bulunduğu
-                    konuma ve günlere bağlı olarak değişiklik
+                    kalma durumunda rezervasyon işleminiz hiçbir şekilde iptal edilmez.</p>
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Otopark fiyatları güncel midir?</h4>
+                <p class="sss-answer">Otopark fiyatları doluluk oranına, otoparkın bulunduğu
+                    sss-questionma ve günlere bağlı olarak değişiklik
                     gösterebilmektedir. Her kullanıcı
                     rezervasyon yaptığı tarifeden ücretlendirilmektedir.</p>
-            </div>
-            <h3 class="soru">Uygulamada problem olduğunda ne yapmalıyım <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> EPS uygulamasında herhangi bir problem ile
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Uygulamada problem olduğunda ne yapmalıyım?</h4>
+                <p class="sss-answer">EPS uygulamasında herhangi bir problem ile
                     karşılaşırsınız uygulama içerisindeki
                     iletişim bölümünden bizlere rahatça ulaşabilirsiniz.</p>
-            </div>
-            <h3 class="soru">Kişisel bilgilerim güvende mi <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> En sıkı güvenlik önlemleri ile kişisel bilgilerinizi
+            </li>
+
+            <li>
+                <h4 class="sss-question"> Kişisel bilgilerim güvende mi?</h4>
+                <p class="sss-answer">En sıkı güvenlik önlemleri ile kişisel bilgilerinizi
                     korumaktayız. Kişisel bilgileriniz
                     hiçbir şekilde başka kurum, kuruluş veya birey ile paylaşılmamaktadır.</p>
-            </div>
-            <h3 class="soru">Park etmek istediğiniz bölgede otopark yoksa ne
-                yapmalıyım <i class="fas fa-question-circle"></i></h3>
-            <div class="cevap">
-                <p><i class="fas fa-arrow-circle-right"></i> Öneri ve Şikâyetlerinizi İletişim bölümünden otopark adını
+            </li>
+
+            <li>
+                <h4 class="sss-question" style="border-bottom-right-radius: 10px;">Park etmek istediğiniz bölgede otopark yoksa ne
+                    yapmalıyım?</h4>
+                <p class="sss-answer">Öneri ve Şikâyetlerinizi İletişim bölümünden otopark adını
                     veya otopark ihtiyacınız bulunan bölgeyi
                     yazarak bize ulaşabilirsiniz. Kısa sürede ekibimiz sizin için istediğiniz noktadan
                     hizmet almanızı sağlayacaktır.</p>
-            </div>
-        </div>
+            </li>
+        </ul>
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+        $(function () {
+            $(".sss-question").on("click", function () {
+                $("ul li p").stop().slideUp();
+                $(this).next("p").stop().slideToggle();
+            })
+        })
+
+</script>
 
 <?php include_once('include/bs-include/end.php'); ?>
