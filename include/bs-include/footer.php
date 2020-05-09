228fd5985e64ce66ad6cda44828160ed28fd3d3d
anilcanboga <60179891+anilcanboga@users.noreply.github.com>
2020-05-09 16:37:51 +0300

    Footer Güncellemesi
    -Footer class adı değiştirildi.

--- include/bs-include/footer.php
+++ include/bs-include/footer.php
@@ -1,5 +1,5 @@
 <?php include_once(__DIR__. '/../functions.php'); ?>
-<div class="footer">
+<div class="footer2">
         <div class="container">
             <!-- container bs -->
             <div>
