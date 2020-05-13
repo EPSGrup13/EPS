baf4866cb555c63047452931e8ee9d74698db2cf
anilcanboga <60179891+anilcanboga@users.noreply.github.com>
2020-05-13 21:06:14 +0300

    Kullanım Koşulları Güncellemesi
    - Metin içeriği değiştirildi.

--- kullanim-kosullari.php
+++ kullanim-kosullari.php
@@ -9,48 +9,63 @@
 
     <div id="kullanim-kutu">
 
-        <p id="kullanim-p"><strong>Başvuru ve Üyelik ;</strong><br>
+        <p id="kullanim-p"><strong>1. Başvuru ve Üyelik ;</strong><br>
             Kullanıcının uyması gereken birtakım yükümlülükler bulunmaktadır: <br><br>
-            Başvuru ve Üyelik <br><br>
-            1.1 Kullanıcı uygulamayı indirip, kayıt için gerekli bilgileri doldurup şifresini belirledikten sonra kayıt
-            olarak sözleşmeyi kabul etmiş sayılır. Kabul ettiği sözleşme kurallarınca sistemi kullanmaya
-            başlayabilir.<br>
-            1.2 Müşteri kayıt anında verdiği tüm bilgileri daima düzgün ve gerçeğe uygun bir biçimde verdiğini kabul
-            eder.<br>
-            1.3 Müşteri rezervasyon ücretini ve otopark işlem tutarını uygulama içerisinden ödemeyi kabul eder.<br>
+            
+            1.1 Kullanıcı EPS mobil uygulama veya web sitemiz üzerinden, sisteme kaydolması için gerekli olan formu doldurup şifresini belirledikten sonra kayıt olmuş olur ve sözleşmeyi kabul etmiş sayılır. Kabul ettiği sözleşme kurallarınca sistemi kullanmaya başlayabilir.<br>
+            1.2 Kullanıcı kayıt esnasında verdiği tüm bilgilerin eksiksiz, gerçek ve güncel olduğunu kabul eder.<br>
+            1.3 Kullanıcı yararlanacağı otopark hizmeti ile ilgili ücreti yalnızca uygulama üzerinden ödeyeceğini kabul etmiş sayılır.<br>
         </p>
     </div>
 
     <div id="kullanim-kutu-2">
 
-        <p id="kullanim-p-2"><strong>Uygulama Kullanımı ;</strong><br><br>
-            2.0.1 Kullanıcı sisteme kayıt olduktan sonra veya giriş yaptıktan sonra seçtiği otoparka rezervasyon yap
-            butonuna tıkladığında, otoparka yer ayırma isteğinde bulunur. Otopark görevlisine kullanıcıya ait araç
-            plakası ve geleceği vakit paylaşılır.<br>
-            • 2.0.2 EPS, otoparka yapılan rezervasyonların onaylanacağı konusunda garanti vermez. Otopark yetkilisi
-            rezervasyonu onaylamadığı taktirde, kullanıcıya rezervasyonun reddedildiği bildirilir.<br>
-            • 2.0.3 Müşteri otopark rezervasyonu yaptığı halde otoparka gitmediği zaman rezervasyon işleminin uygunsuz
-            biçimde kullanılmasını engellemek adına alınacak blokasyon ücreti iade edilmez.<br>
-            • 2.0.4 Müşteri, rezervasyon ücretini ve otopark işlem ücretini sisteme önceden yüklemiş olduğu bakiyeden
-            kullanarak öder.<br>
-            • 2.0.5 Kullanıcının hesabında yeterli miktarda bakiye bulunmadığı tespit edilmediği taktirde rezervasyon
-            işlemi gerçekleştirilmez.<br>
+        <p id="kullanim-p"><strong>2. Uygulama Kullanımı ;</strong><br><br>
+            • 2.0.1 Kullanıcı sisteme giriş yaptıktan sonra rezervasyon yapmak istediği otoparkı seçip, rezervasyon talebinde bulunur. Otopark görevlisinin rezervasyonu onaylaması ile rezervasyon işlemi tamamlanmış sayılır.<br>
+
+            • 2.0.2 EPS, kullanıcıların otoparklara yaptığı rezervasyon taleplerinin kabul edileceği konusunda güvence vermez. Otopark görevlisi rezervasyon işlemini kabul etmediği zaman, kullanıcıya rezervasyonun reddedildiği bildirilir.<br>
+
+            • 2.0.3 Kullanıcı seçtiği otoparka rezervasyon talebi gönderip otopark yetkilisin de talebi onayladığı halde kullanıcı otoparka gitmediği zaman rezervasyon işleminin uygunsuz biçimde kullanılmasını engellemek adına alınacak blokasyon ücreti iade edilmez.<br>
+
+            • 2.0.4 Kullanıcı, otopark rezervasyon ücretini sisteme önceden yüklemiş olduğu bakiyeden kullanarak öder.<br>
+
+            • 2.0.5 Kullanıcının hesabında yeterli miktarda bakiye bulunmadığı tespit edilmediği taktirde rezervasyon işlemi gerçekleştirilmez.<br>
+
             • 2.0.6 Kullanıcının sisteme bakiye yükleme amacıyla kullanacağı kart bilgileri EPS sunucularında tutulmaz.<br>
-            • 2.0.7 Müşteri şikayetçi olduğu otoparkları, uygulama içerisinde bulunan öneri ve şikayet bölümünden
-            yetkili kişilere iletebilir anca EPS bundan sorumlu tutulamaz. Şikayet edilen otoparklar uyarılır ve ilgili
-            konu devam ederse otopark ile EPS arasındaki anlaşma feshedilir.<br>
-            • 2.0.8 Müşteri sistemi hukuka ve amacına uygun şekilde kullanmayı ve sistemde yaptığı her işlem ve
-            eylemdeki hukuki sorumluluğun kendisine ait olduğunu kabul eder. Bu sebepten dolayı EPS doğrudan veya
-            dolaylı olarak hiçbir şekilde sorumlu tutulamaz.<br>
-            • 2.0.9 Kullanıcı, sistemi kullanabilmek için kullandığı kişisel bilgilerinin (Şifre, Telefon vb.)
-            güvenliğini, saklanmasını ve üçüncü kişilerden korunması ile ilgili tüm sorumluluğun kendisine ait olduğunu
-            kabul eder.<br>
-            • 2.1.0 Kullanıcı EPS'nin ücretsiz olarak sunduğu hizmetleri istismar edebilecek herhangi bir işlem ve
-            eylemde bulunmamayı kabul eder.<br>
-            • 2.1.1 Kullanıcı belirtilen koşullara aykırı davrandığı taktirde EPS, üyeliği askıya alma veya üyelikten
-            çıkarma hakkını saklı tutar.<br>
-            • 2.1.2 Kullanıcı sistem içerisinde bulunan destek bölümünden herhangi bir sebep olmaksızın üyeliği iptal
-            edebilir.<br>
+
+            • 2.0.7 Kullanıcılar şikayetçi olduğu otoparklarla ilgili uygulama içerisinde her otoparka ayrılan yorum bölümünde yorum yapabilir ve otoparkları puanlayabilir. Kullanıcı eğer isterse mobil uygulamamız ya da web sitemizde bulunan iletişim bölümünden bizlere ulaşabilir. Çok fazla sayıda şikayet gelen otoparklarla ilgili gerekli çalışmalar yapılır gerekirse otoparklar ile EPS arasındaki anlaşma feshedilebilir.<br>
+
+            • 2.0.8 Kullanıcılar sistemi yasal ve amacı doğrultusunda kullanmayı kabul etmiş sayılır. Yasal ve hukuki olmayan işlemlerden EPS sorumlu tutulamaz, kullanıcıların kendi sorumluluğundadır.<br>
+
+            • 2.0.9 Kullanıcının, EPS sistemini kullanabilmesi için sisteme kaydettiği şahsi bilgilerinin güvenliği, saklanması ve üçüncü şahıs-kurumlarla paylaşılmaması ile ilgili her türlü sorumluluğun kullanıcının kendisinde olduğunu kabul ve beyan eder.<br>
+
+            • 2.1.0 Kullanıcılar EPS'nin sunduğu hizmetleri amacı dışında kullanmamayı ve herhangi bir açıdan istismar etmemeyi kabul eder. EPS, sistemi istismar eden kullanıcıların üyeliklerini dondurabilir ya da silebilir.<br>
+
+            • 2.1.1 Kullanıcılar EPS üyeliklerini herhangi bir sebep göstermeden kendi istekleri ile iptal edebilir.<br>
+
+        </p>
+    </div>
+
+    <div id="kullanim-kutu-3">
+
+    	<p id="kullanim-p"><strong>3. Diğer Şartlar  ;</strong><br><br>
+
+
+    	• 3.1 Fikri Mülkiyet Hakları <br>
+    	○ Mobil uygulamanın ya da internet sitesinin tasarımı, metinleri, tüm kodları ve tüm ögeleri EPS'nin telif hakları kapsamı içerisindedir. Kullanıcılar, EPS hizmetlerini, bilgilerini ticari amaçla kullanması yasaktır. Kullanıcılar EPS'nin izni olmadan, tüm bu telif hakkı içinde bulunan bilgileri ve her türlü ögeyi çoğaltması, dağıtması, 3. Şahıs ya da kurumlarla paylaşması yasaktır. Tüm hakları saklıdır. <br>
+
+    	• 3.2 Sözleşme Değişiklikleri <br>
+    	○ EPS, tek taraflı olarak kendi isteğiyle sözleşmeyi, istediği bir zamanda değiştirme hakkına sahiptir. Sözleşmedeki şartlar değiştikten ve değişiklikler ilan edildiği günden sonra yeni kurallar geçerlidir ve kullanıcılar bunu kabul etmiş sayılır. <br>
+
+    	• 3.3 Mücbir Sebepler <br>
+		○ Her türlü doğal afet (sel, savaş, deprem, yangın vb.), iletişim sorunları, park halinde kaza, kundaklama, arabada maddi hasar gibi durumlarda EPS sorumluluk kabul etmemektedir. Kullanıcıların bu ve benzeri durumlarda tazminat talep etme hakkı yoktur. <br>
+
+		• 3.4 Sözleşmenin Feshi <br>
+		○ Bu koşullar kullanıcının üyeliği sona erinceye kadar EPS ve kullanıcı arasında geçerli sayılacaktır. EPS, kullanıcının aykırı hareket etmesini tespit etmesi durumunda sözleşmeyi tek taraflı feshetme hakkına sahiptir. <br>
+
+		• 3.5 Yürürlük <br>
+		○ Bu sözleşme kullanıcıların elektronik ortamda onaylaması ile birlikte ilgilili taraflarca kabul edilerek yürürlüğe girmiştir
+
         </p>
     </div>
 
