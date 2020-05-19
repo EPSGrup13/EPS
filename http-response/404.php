63df1005613dcf9e9e771d418d846f311ec9cb0f
anilcanboga <60179891+anilcanboga@users.noreply.github.com>
2020-05-19 19:58:20 +0300

    404 Sayfası Güncellemesi

--- http-response/404.php
+++ http-response/404.php
@@ -7,10 +7,14 @@
 	<title>
 	</title>
 </head>
-<body>
 
-Bu sayfa bulunmamaktadır. <!-- Geri Yönlendiriliyorsunuz... -->
-<?php //redirectWithTimer("index"); ?>
-
-</body>
+  <body id="body-404">
+    <div class="sayfa404">
+      <h2>HATA! Bu sayfa bulunmamaktadır.</h2>
+      <h1>404</h1>
+      <p>Aradığınız sayfayı bulamıyoruz.</p>
+      <a class="buton-404" href="http://epark.sinemakulup.com/"> Anasayfaya Dön</a>
+    </div>
+  </body>
+  
 </html>
